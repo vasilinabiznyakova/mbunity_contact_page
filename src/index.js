37d96ac69a34465cc2c52d6cd55ca9e6c74019e2
Vasilina Biznyakova <vbiznyakova@gmail.com>
2023-03-31 05:29:23 +0200

    fix basename

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { App } from "./App";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="mbunity-contact-page">
+    <BrowserRouter basename="mbunity_contact_page">
       <App />
     </BrowserRouter>
   </React.StrictMode>
