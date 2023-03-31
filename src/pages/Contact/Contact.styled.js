bc87145b5cbc2fcc1c6acf0f38883738668bd0ed
Vasilina Biznyakova <vbiznyakova@gmail.com>
2023-04-01 00:00:17 +0200

    fix container for mobile

--- src/pages/Contact/Contact.styled.js
+++ src/pages/Contact/Contact.styled.js
@@ -1,6 +1,9 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  max-width: 1440px;
+  max-width: 360px;
   margin: 0 auto;
+  @media screen and (min-width: 1440px) {
+    max-width: 1440px;
+  }
 `;
