c2265601cfe2c54e36ca7180283a1634d85584bd
Vasilina Biznyakova <vbiznyakova@gmail.com>
2023-03-31 08:07:38 +0200

    added container for the page

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -1,14 +1,15 @@
 import { Header } from "../../components/Header/Header";
 import { ContactSection } from "../../components/ContactSection/ContactSection";
 import { Footer } from "../../components/Footer/Footer";
+import { Container } from "./Contact.styled";
 
 const Contact = () => {
   return (
-    <>
+    <Container>
       <Header />
       <ContactSection />
       <Footer />
-    </>
+    </Container>
   );
 };
 
